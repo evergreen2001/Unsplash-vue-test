7698ace568474cc34d5909fdf156844d99e20c38
evergreen2001 <dicksonevergreenido@gmail.com>
2020-11-24 05:43:11 +0100

    adding modal box

--- src/components/PhotoGrid.vue
+++ src/components/PhotoGrid.vue
@@ -1,59 +1,44 @@
 <template>
   <div class="photogrid">
-    <div class="loading" v-if="isLoading">
+    <div class="loading">
       <div class="photo__row__loader">
         <div class="photo__column__loader">
-          <div class="author__info">
-            <p>username</p>
-            <p>location</p>
+          <div class="author__info__loader">
+            <p></p>
+            <p class="author__info__location__loader"></p>
           </div>
         </div>
 
+<div class="photo__row__loader">
         <div class="photo__column__loader">
-          <div class="author__info">
-            <p>username</p>
-            <p>location</p>
+          <div class="author__info__loader">
+            <p></p>
+            <p class="author__info__location__loader"></p>
+          </div>
+        </div>
+        <div class="photo__row__loader">
+        <div class="photo__column__loader">
+          <div class="author__info__loader">
+            <p></p>
+            <p class="author__info__location__loader"></p>
           </div>
         </div>
 
-        <div class="photo__column__loader">
-          <div class="author__info">
-            <p>username</p>
-            <p>location</p>
-          </div>
-        </div>
-
-        <div class="photo__column__loader">
-          <div class="author__info">
-            <p>username</p>
-            <p>location</p>
-          </div>
-        </div>
-
-        <div class="photo__column__loader">
-          <div class="author__info">
-            <p>username</p>
-            <p>location</p>
-          </div>
-        </div>
-
-        <div class="photo__column__loader">
-          <div class="author__info">
-            <p>username</p>
-            <p>location</p>
-          </div>
-        </div>
+        
       </div>
     </div>
 
-    <!-- <div> <h2> Loading </h2></div> -->
+ 
     <div class="photo__row">
       <div class="photo__column" v-for="item in images" :key="item.id">
+       
         <img :src="item.urls.regular" />
+
 
         <div class="author__info">
           <p>{{item.user.name}}</p>
           <p class="author__location">{{item.user.location}}</p>
+
 
        
         </div>
@@ -97,16 +82,21 @@ export default {
   margin: 0 0 20px 0;
   width: 100%;
   position: relative;
+
+   
 }
 .photo__column > img {
   border-radius: 10px;
   width: 100%;
 
   z-index: 1;
-  transition-duration: 0.3s;
-  transition-property: all;
-  transition-timing-function: ease-out;
-  transform: translateZ(0);
+
+  filter: brightness(0.7);
+ 
+}
+
+.overlay{
+   
 }
 
 .author__info {
@@ -143,7 +133,7 @@ export default {
 
 @media (max-width: 480px) {
   .photo__row {
-    columns: 2;
+    columns: 1;
   }
 }
 
@@ -165,13 +155,48 @@ export default {
   background: #f0f0f0;
   width: 100%;
   border-radius: 20px;
-  animation: shine 1s infinite;
+  position: relative;
+
+  
+  width: 100%;
+    padding-top: 10%;
+    border-radius: 20px;
 }
 
-@keyframes shine {
-  to {
-    background-position: 0 0, 100% 0, /* move highlight to right */ 120px 0,
-      120px 40px, 120px 80px, 120px 120px;
-  }
+.author__info__loader{
+   position: absolute;
+  left: 15px;
+  right: 0;
+  top: 40%;
+  bottom: 27px;
+  display: flex;
+  flex-direction: column;
+  justify-content:flex-end;
+  text-transform: capitalize;
+ 
+  color: red;
+
+
+  
 }
+.author__info__loader > p{
+  background-color: #ddd;
+    width:131px;
+  height:15px;
+  margin-bottom:16px;
+    animation: pulse-bg 1s infinite;
+
+}
+
+.author__info__loader > .author__info__location__loader{
+     width:80px;
+
+
+}
+
+@keyframes pulse-bg {
+    0% { background-color: #ddd; }
+    50% { background-color: #d0d0d0; }
+    100% { background-color: #ddd; }
+  }
 </style>
